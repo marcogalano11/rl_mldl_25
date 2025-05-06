4bdfac7fa39ca31264a275d725b9d041b3ef5aa9
marcogalano11 <galanomarco33@gmail.com>
2025-05-06 17:53:46 +0200

    added sb3 with PPO

--- train_sb3.py
+++ train_sb3.py
@@ -6,6 +6,7 @@
 """
 import gym
 from env.custom_hopper import *
+from stable_baselines3 import PPO
 
 def main():
     train_env = gym.make('CustomHopper-source-v0')
@@ -17,6 +18,37 @@ def main():
     #
     # TASK 4 & 5: train and test policies on the Hopper env with stable-baselines3
     #
+
+    model = PPO("MlpPolicy", train_env, verbose=1)
+
+    model.learn(total_timesteps=100_000)
+
+    model.save("ppo_hopper")
+
+    #If we want to use deletion and reloading
+    """
+        del model
+        model = PPO.load("ppo_hopper")
+    """
+
+
+    #EVALUATION
+    obs = train_env.reset()
+
+    cumulative_reward = 0
+    i = 0
+        
+    while i <10:
+        action, _states = model.predict(obs)
+        obs, reward, done, info = train_env.step(action)
+        cumulative_reward += reward
+        if done: 
+            i += 1
+            print(f"Cumulative reward of episode {i}: {cumulative_reward}")
+            cumulative_reward = 0
+            obs = train_env.reset()
+        
+
 
 if __name__ == '__main__':
     main()
