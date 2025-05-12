87fac8e6880a4c9e35f7a839ff8371948f91bc9b
marcogalano11 <galanomarco33@gmail.com>
2025-05-12 13:53:36 +0200

    sac + sac_policy

--- train_sb3.py
+++ train_sb3.py
@@ -6,7 +6,7 @@
 """
 import gym
 from env.custom_hopper import *
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, SAC
 
 def main():
     train_env = gym.make('CustomHopper-source-v0')
@@ -15,29 +15,45 @@ def main():
     print('Action space:', train_env.action_space)  # action-space
     print('Dynamics parameters:', train_env.get_parameters())  # masses of each link of the Hopper
 
+    ppo_policy = False
+
     #
     # TASK 4 & 5: train and test policies on the Hopper env with stable-baselines3
     #
 
-    model = PPO("MlpPolicy", train_env, verbose=1)
+    if ppo_policy:
 
-    model.learn(total_timesteps=1e6)
+        model = PPO("MlpPolicy", train_env, verbose=1)
 
-    model.save("ppo_hopper")
+        model.learn(total_timesteps=1e6)
 
-    #If we want to use deletion and reloading
-    """
-        del model
-        model = PPO.load("ppo_hopper")
-    """
+        model.save("ppo_hopper")
 
+        #If we want to use deletion and reloading
+        """
+            # del model #this only if we have trained a model in this script and we want to delete it
+            model = PPO.load("ppo_hopper")
+        """
+    else:
+
+        model = SAC("MlpPolicy", train_env, verbose=1)
+
+        model.learn(total_timesteps=500_000, log_interval=4)
+
+        model.save("sac_hopper")
+
+        #If we want to use deletion and reloading
+        """
+            # del model #this only if we have trained a model in this script and we want to delete it
+            model = SAC.load("sac_hopper")
+        """
 
     #EVALUATION
     obs = train_env.reset()
 
     cumulative_reward = 0
     i = 0
-        
+            
     while i <10:
         action, _states = model.predict(obs)
         obs, reward, done, info = train_env.step(action)
