c2058e82ca92970e5df8fe07fdabab49bfec05b8
aliambr <s345061@studenti.polito.it>
2025-05-30 18:52:17 +0200

    divisione reinforce e actor critic

---
+++ agent_reinforce.py
@@ -0,0 +1,117 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch.distributions import Normal
+
+
+def discount_rewards(r, gamma):
+    discounted_r = torch.zeros_like(r)
+    running_add = 0
+    for t in reversed(range(0, r.size(-1))):
+        running_add = running_add * gamma + r[t]
+        discounted_r[t] = running_add
+    return discounted_r
+
+
+class Policy(torch.nn.Module):
+    def __init__(self, state_space, action_space):
+        super().__init__()
+        self.state_space = state_space
+        self.action_space = action_space
+        self.hidden = 64
+        self.tanh = torch.nn.Tanh()
+
+        """
+            Actor network
+        """
+        self.fc1_actor = torch.nn.Linear(state_space, self.hidden)
+        self.fc2_actor = torch.nn.Linear(self.hidden, self.hidden)
+        self.fc3_actor_mean = torch.nn.Linear(self.hidden, action_space)
+        
+        # Learned standard deviation for exploration at training time 
+        self.sigma_activation = F.softplus
+        init_sigma = 0.5
+        self.sigma = torch.nn.Parameter(torch.zeros(self.action_space)+init_sigma)
+
+        self.init_weights()
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if type(m) is torch.nn.Linear:
+                torch.nn.init.normal_(m.weight)
+                torch.nn.init.zeros_(m.bias)
+
+
+    def forward(self, x):
+        x_actor = self.tanh(self.fc1_actor(x))
+        x_actor = self.tanh(self.fc2_actor(x_actor))
+        action_mean = self.fc3_actor_mean(x_actor)
+
+        sigma = self.sigma_activation(self.sigma)
+        normal_dist = Normal(action_mean, sigma)
+        
+        return normal_dist
+
+
+class Agent(object):
+    def __init__(self, policy, device='cpu'):
+        self.train_device = device
+        self.policy = policy.to(self.train_device)
+        self.optimizer = torch.optim.Adam(policy.parameters(), lr=1e-3)
+        self.gamma = 0.99
+        self.states = []
+        self.next_states = []
+        self.action_log_probs = []
+        self.rewards = []
+
+
+    def update_policy(self):
+        action_log_probs = torch.stack(self.action_log_probs, dim=0).to(self.train_device).squeeze(-1)
+        rewards = torch.stack(self.rewards, dim=0).to(self.train_device).squeeze(-1)
+        
+        self.states, self.next_states, self.action_log_probs, self.rewards, self.done , self.values = [], [], [], [], [], []
+
+        #
+        # TASK 2:
+        #   - compute discounted returns
+        #   - compute policy gradient loss function given actions and returns
+        #   - compute gradients and step the optimizer
+        #
+
+        #using the predefined discounted_rewards function to compute discounted rewards from the current rewards and the defined gamma value
+        discounted_returns = discount_rewards(rewards, self.gamma)
+        b = 0
+        returns = discounted_returns - b
+
+        #compute the policy loss
+        policy_loss = -torch.sum(action_log_probs * returns)
+
+        #gradient and step optimizer
+        self.optimizer.zero_grad()
+        policy_loss.backward()
+        self.optimizer.step()       
+
+
+    def get_action(self, state, evaluation=False):
+        """ state -> action (3-d), action_log_densities """
+        x = torch.from_numpy(state).float().to(self.train_device)
+
+        normal_dist = self.policy(x)
+
+        if evaluation:  # Return mean
+            return normal_dist.mean, None
+
+        action = normal_dist.sample()
+
+        # Compute Log probability of the action [ log(p(a[0] AND a[1] AND a[2])) = log(p(a[0])*p(a[1])*p(a[2])) = log(p(a[0])) + log(p(a[1])) + log(p(a[2])) ]
+        action_log_prob = normal_dist.log_prob(action).sum()
+
+        return action, action_log_prob
+
+
+    def store_outcome(self, state, next_state, action_log_prob, reward):
+        self.states.append(torch.from_numpy(state).float())
+        self.action_log_probs.append(action_log_prob)
+        self.rewards.append(torch.Tensor([reward]))
+
---
+++ train_reinforce.py
@@ -0,0 +1,82 @@
+"""Train an RL agent on the OpenAI Gym Hopper environment using
+    REINFORCE algorithm
+"""
+import argparse
+import torch
+import gym
+from env.custom_hopper import *
+from agent_reinforce import Agent, Policy
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n-episodes', default=200_000, type=int, help='Number of training episodes')
+    parser.add_argument('--print-every', default=20000, type=int, help='Print info every <> episodes')
+    parser.add_argument('--device', default='cpu', type=str, help='network device [cpu, cuda]')
+
+    return parser.parse_args()
+def save_plot_rewards(reward_list, title="REINFORCE Training Performance", filename="reinforce_rewards.png"):
+    episodes = np.arange(1, len(reward_list)+1)
+    plt.plot(episodes, reward_list)
+    plt.title(title)
+    plt.xlabel("Episode")
+    plt.ylabel("Reward")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(filename)
+    plt.close()
+
+
+args = parse_args()
+
+
+def main():
+
+	env = gym.make('CustomHopper-source-v0')
+	# env = gym.make('CustomHopper-target-v0')
+
+	print('Action space:', env.action_space)
+	print('State space:', env.observation_space)
+	print('Dynamics parameters:', env.get_parameters())
+
+	"""
+		Training
+	"""
+	observation_space_dim = env.observation_space.shape[-1]
+	action_space_dim = env.action_space.shape[-1]
+
+	policy = Policy(observation_space_dim, action_space_dim)
+	agent = Agent(policy, device=args.device)
+
+    #
+    # TASK 2 and 3: interleave data collection to policy updates
+    #
+	reward_list = []  
+
+	for episode in range(args.n_episodes):
+		done = False
+		train_reward = 0
+		state = env.reset()  # Reset the environment and observe the initial state
+		while not done:  # Loop until the episode is over
+
+			action, action_probabilities = agent.get_action(state, False)
+			previous_state = state
+			state, reward, done, info = env.step(action.detach().cpu().numpy())
+			agent.store_outcome(previous_state, state, action_probabilities, reward)
+			train_reward += reward
+		
+		agent.update_policy()
+		reward_list.append(train_reward)
+		if (episode+1)%args.print_every == 0:
+			print('Training episode:', episode)
+			print('Episode return:', train_reward)
+
+
+	torch.save(agent.policy.state_dict(), "reinforce.mdl")
+	save_plot_rewards(reward_list, title="REINFORCE Training Performance", filename="reinforce_rewards.png")
+	
+
+if __name__ == '__main__':
+	main()
